4bbe22e753aa93a3566876702e01fc68f65c0f9c
Samuel Palacios Bernate <sdpalaciob@eafit.edu.co>
2021-11-01 16:33:54 -0500

    Add Header for disk code

--- p3/include/disk.h
+++ p3/include/disk.h
@@ -1,1 +1,7 @@
-//function declaration
+#include <stdbool.h>
+
+// Function declaration
+extern void fcfs_disk(int head, int requests[]);
+extern void sstf(int head, int requests[]);
+extern void scan(int head, int requests[], size_t reqSize, bool Rmovement);
+extern void cscan(int head, int requests[], size_t reqSize, bool Rmovement);
