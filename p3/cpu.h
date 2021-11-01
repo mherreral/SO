c060c249ecbbb2e177b3b4a1dfc26ee42522dd1b
Manuela <mherreral@eafit.edu.co>
2021-11-01 11:24:53 -0500

    added header

---
+++ p3/cpu.h
@@ -0,0 +1,5 @@
+extern void fcfs(ProcessStruct processes[], int nproc);
+extern void sjf(ProcessStruct processes[], int nproc);
+extern void priorityNP(ProcessStruct processes[], int nproc);
+extern void rr(ProcessStruct processes[], int nproc, int q);
+
